9a3b6f24e31031f614de5cdbc46aa465f6f49749
Finn S <finnstainton@gmail.com>
2019-10-23 14:42:15 +1300

    Updated Styling and Content

---
+++ index.php
@@ -0,0 +1,94 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Personal Website">
+        <meta name="author" content="Finn Stainton">
+        <title>Finn Stainton</title>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="css/all.css" rel="stylesheet">
+        <script defer src="js/all.js"></script>
+    </head>
+    <body class="d-flex flex-column h-100">
+        <main role="main">
+            <div class="jumbotron">
+                <div class="container">
+                    <nav class="navbar navbar-light float-right">
+                        <a href="https://www.github.com/finn-stainton/"><i class="fab fa-github"></i></a>
+                    </nav>
+                    <h1 class="display-3">Finn Stainton</h1>
+                    <p>Student of Life. Bachelor of Computer & Information Sciences<small>*</small>. Art & Electronics hobbyist.</p>
+                </div>
+            </div>
+
+            <section id="about" style="padding: 25px;">
+                <div class="container">
+                    <div class="row text-center mb-4">
+                        <div class="col">
+                            <span class="h2 d-block">person.about();</span>
+                        </div>
+                    </div>
+                    <div class="text-center">
+                        <p>Text here</p>
+                    </div>
+                </div>
+            </section>
+            <!--
+            <section id="projects" style="padding: 25px;" class="bg-dark">
+                <div class="container">
+                    <div class="row text-center mb-4">
+                        <div class="col">
+                            <span class="h2 d-block text-light">Projects.</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="card bg-dark text-light">
+                                <img src="..." class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Project Title</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-secondary">Badge</span></h6>
+
+                                    <p class="card-text">Body text.</p>
+                                    <a href="#" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="articles" style="padding: 25px;">
+                <div class="container">
+                    <div class="row text-center mb-4">
+                        <div class="col">
+                            <span class="h2 d-block">Articles.</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="card">
+                                <img src="..." class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Article Title</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-secondary">Badge</span></h6>
+
+                                    <p class="card-text">Body text.</p>
+                                    <a href="#" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            -->
+        </main>
+        <footer class="footer mt-auto py-3 bg-dark sticky">
+            <div class="container">
+                <span class="text-muted">Crafted by Finn Stainton 2019. Styled with <a href="https://www.getbootstrap.com">bootstrap</a> and <a href="https://www.fontawesome.com">fontawesome</a></span>
+                <small class="text-muted float-right">* in progress</small>
+            </div>
+        </footer>
+
+    </body>
+</html>
